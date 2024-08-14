ff436df74932dff63aec2901897d219a96ecf9db
sickdyd <roberto.reale.ja@gmail.com>
2024-08-14 15:06:49 +0900

    Update main page copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,52 +4,61 @@ import Quote from "./components/Quote";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col max-w-screen-md m-auto w-full mt-12">
+    <main className="flex flex-col max-w-screen-md m-auto p-6 pt-12 bg-white bg-opacity-90 h-full">
       <Quote
         cite="https://quoteinvestigator.com/2015/04/02/simple/"
         quote="Simplicity is the ultimate sophistication."
-        author="Leonardo Da Vinci"
+        author="Unknown"
       />
 
-      <Paragraph>👋 Hello, welcome to my homepage!</Paragraph>
+      <Paragraph>👋 Hi, I'm Roberto Reale. Welcome!</Paragraph>
       <Paragraph>
-        Throughout my career, I have honed my skills in various programming
-        languages and frameworks. I work mainly with <strong>JavaScript</strong>{" "}
-        (React, Next.js, Node.js) and <strong>Ruby on Rails</strong>. I am
-        proficient with <strong>HTML</strong> and <strong>CSS</strong> (vanilla
-        and Tailwind). I am also a{" "}
+        I’m a web developer specializing in React, Next.js, Node.js, and Ruby on
+        Rails, with a solid foundation in HTML and CSS (including Tailwind).
+      </Paragraph>
+      <Paragraph>
+        I am proud to be a{" "}
         <Link
           target="_blank"
-          href="https://www.credly.com/badges/08104359-709a-4150-b3b7-297d90f26ccf/Paragraphublic_url"
+          href="https://www.credly.com/badges/08104359-709a-4150-b3b7-297d90f26ccf/public_url"
           aria-label="Certified Kubernetes Administrator badge on Credly"
         >
-          <strong>Certified Kubernetes Administrator</strong>
+          Certified Kubernetes Administrator
         </Link>
         .
       </Paragraph>
       <Paragraph>
-        In my spare time I am contribuiting with a few{" "}
+        In my free time I contribute to the Open Source community with various{" "}
         <Link
           target="_blank"
           href="https://www.npmjs.com/~sickdyd"
           aria-label="npm packages on npmjs.com"
         >
           npm packages
+        </Link>
+        .
+      </Paragraph>
+      <Paragraph>
+        Based in the vibrant city of Osaka, I’m always looking to connect with
+        like-minded professionals and enthusiasts. For that reason, I created a{" "}
+        <Link
+          href="https://www.meetup.com/osaka-digital-nomads-meetup-group/"
+          target="blank"
+          aria-label="digital nomads meetup"
+        >
+          weekly meetup event
         </Link>{" "}
-        to the Open Source Community.
+        that connects developers from all over the world.
       </Paragraph>
       <Paragraph>
-        I am located in Osaka, <strong>Japan</strong>.
-      </Paragraph>
-      <Paragraph>
-        If you want to get in touch, hit me up with an{" "}
+        Feel free to reach out! Drop me an{" "}
         <Link
           href="mailto:roberto.reale.ja@gmail.com"
           aria-label="Send email to Roberto Reale"
         >
           email
         </Link>{" "}
-        :-)
+        and let’s start a conversation. :-)
       </Paragraph>
     </main>
   );
