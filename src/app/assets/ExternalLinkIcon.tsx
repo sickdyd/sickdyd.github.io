c723e57b0aaa24a2ecb26db54cf7340bd4cbd6cf
sickdyd <roberto.reale.ja@gmail.com>
2024-08-15 17:05:08 +0900

    Make external link icon smaller

--- src/app/assets/ExternalLinkIcon.tsx
+++ src/app/assets/ExternalLinkIcon.tsx
@@ -2,8 +2,8 @@ export default function ExternalLinkIcon() {
   return (
     <svg
       className="ml-0.5 inline"
-      width="16px"
-      height="16px"
+      width="12px"
+      height="12px"
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
