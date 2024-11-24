4cb28b0417654eb0be8f3d462c9c35f5cfe4e327
sickdyd <roberto.reale.ja@gmail.com>
2024-11-24 15:47:22 +0900

    Fix mobile menu

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,43 +1,94 @@
+"use client";
 import { Inter } from "next/font/google";
-import NextLink from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
 import GithubIcon from "../assets/GithubIcon";
+import NavbarEntry from "./NavbarEntry";
 
 const inter = Inter({ subsets: ["latin"], weight: ["700"] });
 
+export const NOT_EXPANDED = "sm:max-h-14";
+
 export default function Navbar() {
+  const [maxHeightClassName, setMaxHeightClassName] = useState(NOT_EXPANDED);
+  const pathname = usePathname();
+  const navRef = useRef<HTMLElement | null>(null);
+
+  const toggleMenu = () => {
+    maxHeightClassName === NOT_EXPANDED
+      ? setMaxHeightClassName("sm:max-h-full")
+      : setMaxHeightClassName(NOT_EXPANDED);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        navRef.current &&
+        event.target instanceof Node &&
+        !navRef.current.contains(event.target)
+      ) {
+        setMaxHeightClassName(NOT_EXPANDED);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <nav
+      ref={navRef}
       className={`fixed bg-quaternary font-bold text-white ${inter.className} w-full shadow-md z-10`}
     >
-      <ul className="m-auto flex max-w-3xl items-center justify-between gap-6 p-4 text-sm font-bold uppercase text-quinary">
-        <li className="mr-auto">
-          <NextLink className="link" href="/">
+      <ul
+        className={`m-auto flex max-w-3xl items-center justify-between gap-6 p-4 text-sm font-bold uppercase text-quinary transition-all duration-300 ease-in-out sm:m-0 sm:max-w-fit sm:flex-col sm:items-start sm:overflow-hidden ${maxHeightClassName}`}
+      >
+        <li className="hidden sm:block">
+          <span onClick={toggleMenu}>Menu</span>
+        </li>
+        <li className="mr-auto sm:mr-0">
+          <NavbarEntry href="/" toggleMenu={toggleMenu} pathname={pathname}>
             Roberto Reale
-          </NextLink>
+          </NavbarEntry>
         </li>
         <li>
-          <NextLink className="link" href="/recent-work">
+          <NavbarEntry
+            href="/recent-work"
+            toggleMenu={toggleMenu}
+            pathname={pathname}
+          >
             Recent work
-          </NextLink>
+          </NavbarEntry>
         </li>
         <li>
-          <NextLink className="link" href="/projects">
+          <NavbarEntry
+            href="/projects"
+            toggleMenu={toggleMenu}
+            pathname={pathname}
+          >
             Projects
-          </NextLink>
+          </NavbarEntry>
         </li>
         <li>
-          <NextLink className="link" href="mailto:roberto.reale.ja@gmail.com">
+          <NavbarEntry
+            href="mailto:roberto.reale.ja@gmail.com"
+            toggleMenu={toggleMenu}
+            pathname={pathname}
+          >
             Email
-          </NextLink>
+          </NavbarEntry>
         </li>
         <li>
-          <NextLink
-            className="link"
+          <NavbarEntry
             href="https://github.com/sickdyd"
+            toggleMenu={toggleMenu}
+            pathname={pathname}
             target="_blank"
           >
             <GithubIcon />
-          </NextLink>
+          </NavbarEntry>
         </li>
       </ul>
     </nav>
---
+++ src/app/components/NavbarEntry.tsx
@@ -0,0 +1,31 @@
+import NextLink from "next/link";
+import { ReactNode } from "react";
+
+export default function NavbarEntry({
+  children,
+  href,
+  target = "_self",
+  toggleMenu,
+  pathname,
+}: {
+  children: ReactNode;
+  href: string;
+  pathname: string;
+  toggleMenu: () => void;
+  target?: string;
+}) {
+  const Arrow = () => <span className="hidden sm:mr-1 sm:block">-&gt;</span>;
+
+  console.log({ pathname, href });
+
+  return (
+    <NextLink className="link" target={target} href={href} onClick={toggleMenu}>
+      <span className="flex items-center whitespace-nowrap">
+        {(pathname === href || (pathname.includes(href) && href !== "/")) && (
+          <Arrow />
+        )}
+        {children}
+      </span>
+    </NextLink>
+  );
+}
